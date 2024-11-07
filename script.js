4e25a1ad7c0f43ccf5615d4e78aec808e4554e7c
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-07 04:35:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function makeActive(id) {
     sections.forEach(section => {
         section.classList.remove('active');
     });
-    document.getElementById(id).classList.add('active');
+    document.querySelector(`[onclick="makeActive('${id}')"]`).classList.add('active');
 }
 
 function searchSite() {
