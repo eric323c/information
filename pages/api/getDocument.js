0960a50b0e3a8cebfbffd6211669a5f1a7fdcb27
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-08 04:41:26 -0500

    Update getDocument.js

--- pages/api/getDocument.js
+++ pages/api/getDocument.js
@@ -1,14 +1,11 @@
-const clientPromise = require('../../db');
+const connectToDatabase = require('../../db');
 
 export default async function handler(req, res) {
-    try {
-        const client = await clientPromise;
-        const db = client.db("ThirdShiftHub"); // Specify your database name
-
-        const documents = await db.collection("documents").find({}).toArray();
-        res.status(200).json(documents);
-    } catch (error) {
-        console.error("Error fetching documents:", error);
-        res.status(500).json({ error: "Failed to fetch documents" });
+    if (req.method !== 'GET') {
+        return res.status(405).send({ message: 'Only GET requests are allowed' });
     }
+
+    const { db } = await connectToDatabase();
+    const documents = await db.collection('Documents').find({}).toArray();
+    res.status(200).json(documents);
 }
