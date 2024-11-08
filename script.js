0ff9b0e94c76a722502549c1a89329a547ac669e
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-08 06:41:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -81,8 +81,47 @@ function downloadDocument() {
 function openVAGuide() {
     document.getElementById('vaGuideModal').style.display = 'block';
 }
+// Function to open the VA Guide modal
+function openVAGuide() {
+    const modal = document.getElementById('vaGuideModal');
+    modal.style.display = 'block';
+    dragElement(modal); // Make modal draggable
+}
 
 // Function to close the VA Guide modal
 function closeVAGuide() {
     document.getElementById('vaGuideModal').style.display = 'none';
+}
+
+// Function to make the modal draggable
+function dragElement(element) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    const header = element.querySelector(".modal-header");
+    if (header) {
+        header.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
