67fabe8ab6dfbb98a355eddded6f23f4fc252fd5
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-08 01:31:55 -0500

    Update getDocument.js

--- pages/api/getDocument.js
+++ pages/api/getDocument.js
@@ -1,16 +1,14 @@
-// getDocument.js
-
-const clientPromise = require('./db');
+import clientPromise from '../../db';
 
 export default async function handler(req, res) {
-  try {
-    const client = await clientPromise;
-    const db = client.db("ThirdShiftHub"); // Use your database name
+    try {
+        const client = await clientPromise;
+        const db = client.db("ThirdShiftHub");
 
-    const documents = await db.collection("documents").find({}).toArray();
-    res.status(200).json(documents);
-  } catch (error) {
-    console.error("Error fetching documents:", error);
-    res.status(500).json({ error: "Failed to fetch documents" });
-  }
+        const documents = await db.collection("documents").find({}).toArray();
+        res.status(200).json(documents);
+    } catch (error) {
+        console.error("Error fetching documents:", error);
+        res.status(500).json({ error: "Failed to fetch documents" });
+    }
 }
