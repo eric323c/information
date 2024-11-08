3eb36e1ccb08f3bfbfffc50f54301f0efd5b9eab
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-08 06:52:29 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
-// Function to check if the entered password is correct
+// login.js
+
 function checkPassword() {
     const passwordInput = document.getElementById('passwordInput').value;
     const correctPassword = "PsuMapa2017";
-    
+
     if (passwordInput === correctPassword) {
         // Set a flag to indicate the user is logged in
         localStorage.setItem("isLoggedIn", "true");
-        
+
         // Redirect to the main page
         window.location.href = "index.html";
     } else {
