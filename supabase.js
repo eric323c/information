a9a2379336392b63a77eea83e7d6674b0f9e1bf3
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-16 01:58:53 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,4 @@
-import { createClient } from './supabase.js'; // Ensure this path matches the actual location
-
 const supabaseUrl = 'https://ydadnbbobjvwusyjgaxa.supabase.co'; // Replace with your Supabase URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkYWRuYmJvYmp2d3VzeWpnYXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MzQwNzAsImV4cCI6MjA0NzMxMDA3MH0.PC43GVqiDoVgLhHfh-ibh7hEISG88YGeyk2Nz4XUthk'; // Replace with your Supabase API key
 export const supabase = createClient(supabaseUrl, supabaseKey);
+window.supabase = supabase;
