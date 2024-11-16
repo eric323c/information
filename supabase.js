3d0f579807782e2a0c5081c574452bf7b2ff6f02
eric323c <71191021+eric323c@users.noreply.github.com>
2024-11-16 02:07:46 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,8 @@
-const supabaseUrl = 'https://ydadnbbobjvwusyjgaxa.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkYWRuYmJvYmp2d3VzeWpnYXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MzQwNzAsImV4cCI6MjA0NzMxMDA3MH0.PC43GVqiDoVgLhHfh-ibh7hEISG88YGeyk2Nz4XUthk'; // Replace with your Supabase API key
+// supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+// Your Supabase API URL and Public Key (Anon Key)
+const supabaseUrl = 'https://ydadnbbobjvwusyjgaxa.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkYWRuYmJvYmp2d3VzeWpnYXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MzQwNzAsImV4cCI6MjA0NzMxMDA3MH0.PC43GVqiDoVgLhHfh-ibh7hEISG88YGeyk2Nz4XUthk';
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
-window.supabase = supabase;
